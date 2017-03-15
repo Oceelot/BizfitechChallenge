c9841011e513e0cbfe642285a478e21362dc1e74
Gary Adams <Gary.Adams@advancedcomputersoftware.com>
2017-03-15 22:01:51 +0000

    added language configuration

--- DataAccess/DataAccess/Configurations/LanguageConfiguration.cs
+++ DataAccess/DataAccess/Configurations/LanguageConfiguration.cs
@@ -1,7 +1,15 @@
-﻿namespace DataAccess.Configurations
+﻿using System.Data.Entity.ModelConfiguration;
+using Domain.Languages;
+
+namespace DataAccess.Configurations
 {
-    public class LanguageConfiguration
+    public class LanguageConfiguration : EntityTypeConfiguration<Language>
     {
-         
+        public LanguageConfiguration()
+        {
+            HasKey(l => l.Id);
+
+            Property(l => l.Description).IsRequired().HasMaxLength(255);
+        }
     }
 }
