4d6b718b1325ce9add8092346e98d74fd0ae847e
Gary Adams <Gary.Adams@advancedcomputersoftware.com>
2017-03-15 21:46:50 +0000

    removed unused class, addded config files for ef

---
+++ DataAccess/DataAccess/Configurations/LanguageConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace DataAccess.Configurations
+{
+    public class LanguageConfiguration
+    {
+         
+    }
+}
---
+++ DataAccess/DataAccess/Configurations/ProgrammerConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Domain.Programmers;
+
+namespace DataAccess.Configurations
+{
+    public class ProgrammerConfiguration : EntityTypeConfiguration<Programmer>
+    {
+        public ProgrammerConfiguration()
+        {
+            HasKey(p => p.Id);
+            Property(p => p.Bio).HasMaxLength(255);
+            Property(p => p.BlogUrl).HasMaxLength(255);
+        }
+    }
+}
