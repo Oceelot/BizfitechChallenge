fab9a66f4a066de20dbdc5aa5ce005cc8864426c
Gary Adams <Gary.Adams@advancedcomputersoftware.com>
2017-03-15 23:39:29 +0000

    created the integrations test project, and nunit need todo testhelpers next

---
+++ DataAccess.Tests.Integration/IntegrationTestSetUp.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace DataAccess.Tests.Integration
+{
+    [SetUpFixture]
+    public class IntegrationTestSetUp
+    {
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [OneTimeTearDown]
+        public void Teardown()
+        {
+            
+        }
+    }
+}
