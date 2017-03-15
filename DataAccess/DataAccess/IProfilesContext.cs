4e9f2b551d8ada5a6125504595af162deed6bf63
Gary Adams <Gary.Adams@advancedcomputersoftware.com>
2017-03-15 22:46:13 +0000

    started a profiles context

---
+++ DataAccess/DataAccess/IProfilesContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+using System.Linq;
+using Domain;
+
+namespace DataAccess
+{
+    public interface IProfilesContext
+    {
+        IDbConnection Connection { get; }
+        IQueryable<T> GetEntities<T>() where T : Entity;
+    }
+}
---
+++ DataAccess/DataAccess/ProfilesContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using Domain;
+
+namespace DataAccess
+{
+    public class ProfilesContext : DbContext, IProfilesContext
+    {
+        public ProfilesContext()
+        {
+            Database.SetInitializer<ProfilesContext>(null);
+        }
+
+        public IDbConnection Connection => Database.Connection;
+
+        public IQueryable<T> GetEntities<T>() where T : Entity 
+        {
+            return Set<T>();
+        } 
+    }
+}
