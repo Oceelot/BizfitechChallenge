8512f8bad208fafed890aaff49865ef9d076a10c
Gary Adams <Gary.Adams@advancedcomputersoftware.com>
2017-03-15 21:15:53 +0000

    added a programmer domain object

---
+++ Domain/Domain/Programmers/Programmer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Domain.Programmers
+{
+    public class Programmer
+    {
+        public Guid Id { get; set; }
+        public string Bio { get; set; }
+        public string BlogUrl { get; set; }
+    }
+}
